f19a4d93bb83965e11ebbf5d4c3b4b5c421f6cdd
Felix Lamadrid <felix.lamadrid97@gmail.com>
2024-06-04 09:35:12 -0500

    chore(docker-compose.yml): optimize volume mounting by adding 'cached' option refactor(vite.config.js): clean up comments and reformat input array for laravel plugin

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     server: {
         host: '0.0.0.0',
-        // hrm es para que se recargue la pagina automaticamente
-        // protocol es para que sepa que es un protocolo de websockets
         hmr: {
             host: 'localhost',
             protocol: 'ws',
@@ -13,8 +11,12 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/views/**/*.blade.php', 'resources/img/**/*'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/img/**/*'
+            ],
             refresh: true,
         }),
-    ],
+    ]
 });
